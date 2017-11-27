0e9dee275f1e72523582a5f458cff1714adca6e8
pikapema <pikapema@gmail.com>
2017-11-27 10:53:44 +0100

    First commit

---
+++ CustomVisionUpload/CustomVisionUpload/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CustomVisionUpload
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
